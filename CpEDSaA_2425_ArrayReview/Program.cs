267c1310ab81dc589c333a375ab5dfcdf6b494a5
Julian Laspona <JulianDevAcc@hotmail.ph>
2024-09-09 13:29:04 +0800

    Add project files.

---
+++ CpEDSaA_2425_ArrayReview/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CpEDSaA_2425_ArrayReview
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uInput = "Southville International University";
+            char[] chars = uInput.ToCharArray();
+
+            //Console.WriteLine(uInput);
+            //foreach(char c in uInput)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            Console.WriteLine(uInput[7]);
+            Console.WriteLine(chars[7]);
+
+            for(int x = 0;  x < chars.Length; x++)
+            {
+                if(x % 2 == 0)
+                {
+                    if ((int)chars[x] >= 97)
+                    {
+                        chars[x] = (char)((int)chars[x] - 32);
+                    }
+                    else
+                    {
+                        chars[x] = (char)((int)chars[x] + 32);
+                    }
+                }
+            }
+            Console.WriteLine(uInput);
+            foreach (char c in chars)
+            {
+                Console.Write(c);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
